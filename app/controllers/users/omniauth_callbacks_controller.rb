3b2a454d96d9689f0e6f41bd104cad6fe32420f7
Nina Breznik <ninabreznik@gmail.com>
2015-07-29 06:18:28 +0200

    Modify omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,7 +16,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
       @user = User.from_omniauth(request.env["omniauth.auth"])
-      sign_in_and_redirect @user
+      sign_in  @user
     end
 
 
