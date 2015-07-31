5a60766b03428b461aa0ff64b8ec335c3ee090a5
Nina Breznik <ninabreznik@gmail.com>
2015-07-31 03:13:41 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
     resources :conversations, only: [:index, :show, :new, :create] do
       member do
         post :reply
