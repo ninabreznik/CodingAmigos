f240b42ad8bdb46ef7e6c8b8e51800a61da3b307
Nina Breznik <ninabreznik@gmail.com>
2015-08-15 02:30:46 +0200

    Modify application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,55 +14,57 @@
 //= require jquery_ujs
 //= require_tree .
 
-// $(function() {
-//   $('.directUpload').find("input:file").each(function(i, elem) {
-//     var fileInput    = $(elem);
-//     var form         = $(fileInput.parents('form:first'));
-//     var submitButton = form.find('input[type="submit"]');
-//     var progressBar  = $("<div class='bar'></div>");
-//     var barContainer = $("<div class='progress'></div>").append(progressBar);
-//     fileInput.after(barContainer);
-//     fileInput.fileupload({
-//       fileInput:       fileInput,
-//       url:             '<%= @s3_direct_post.url %>',
-//       type:            'POST',
-//       autoUpload:       true,
-//       formData:         <%= @s3_direct_post.fields.to_json.html_safe %>,
-//       paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-//       dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
-//       replaceFileInput: false,
-//       progressall: function (e, data) {
-//         var progress = parseInt(data.loaded / data.total * 100, 10);
-//         progressBar.css('width', progress + '%')
-//       },
-//       start: function (e) {
-//         submitButton.prop('disabled', true);
-//
-//         progressBar.
-//           css('background', 'green').
-//           css('display', 'block').
-//           css('width', '0%').
-//           text("Loading...");
-//       },
-//       done: function(e, data) {
-//         submitButton.prop('disabled', false);
-//         progressBar.text("Uploading done");
-//
-//         // extract key and generate URL from response
-//         var key   = $(data.jqXHR.responseXML).find("Key").text();
-//         var url   = '//<%= @s3_direct_post.url.host %>/' + key;
-//
-//         // create hidden field
-//         var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
-//         form.append(input);
-//       },
-//       fail: function(e, data) {
-//         submitButton.prop('disabled', false);
-//
-//         progressBar.
-//           css("background", "red").
-//           text("Failed");
-//       }
-//     });
-//   });
-// });
+/*
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput    = $(elem);
+    var form         = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar  = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+    fileInput.fileupload({
+      fileInput:       fileInput,
+      url:             '<%= @s3_direct_post.url %>',
+      type:            'POST',
+      autoUpload:       true,
+      formData:         <%= @s3_direct_post.fields.to_json.html_safe %>,
+      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
+      replaceFileInput: false,
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressBar.css('width', progress + '%')
+      },
+      start: function (e) {
+        submitButton.prop('disabled', true);
+
+        progressBar.
+          css('background', 'green').
+          css('display', 'block').
+          css('width', '0%').
+          text("Loading...");
+      },
+      done: function(e, data) {
+        submitButton.prop('disabled', false);
+        progressBar.text("Uploading done");
+
+        // extract key and generate URL from response
+        var key   = $(data.jqXHR.responseXML).find("Key").text();
+        var url   = '//<%= @s3_direct_post.url.host %>/' + key;
+
+        // create hidden field
+        var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
+        form.append(input);
+      },
+      fail: function(e, data) {
+        submitButton.prop('disabled', false);
+
+        progressBar.
+          css("background", "red").
+          text("Failed");
+      }
+    });
+  });
+});
+*/
