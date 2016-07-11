22b5adf95dc974f0e8de16147b10d5fde01e2ab9
ninabreznik <ninabreznik@gmail.com>
2016-07-11 05:03:28 +0200

    wookmark

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427011126) do
+ActiveRecord::Schema.define(version: 20160429224417) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 20160427011126) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
-    t.string   "description"
+    t.text     "description",          limit: 255
     t.string   "email"
     t.string   "category"
     t.string   "live_demo"
